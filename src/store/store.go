1f2c17b4d173010f4a6d326e1267592004b14be6
martin on edward <m.e@acm.org>
2017-02-10 22:07:06 +0800

    interfaces for store

--- src/store/store.go
+++ src/store/store.go
@@ -1,1 +1,14 @@
 package store
+
+type Store interface {
+	Add(storable Storable)
+	Changed(storable Storable)
+	Get(ident string) (storable Storable, found bool)
+	Close()
+}
+
+type Storable interface {
+	Ident() (ident string)
+}
+
+type StoreHelper interface{}
