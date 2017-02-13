5bf1c0cb3cf8a7e877aa7351c64ac1ecfddf04eb
Martin Ellison <m.e@acm.org>
2017-02-13 13:38:54 +0800

    tests for store

--- src/pagestore/pagestore.go
+++ src/pagestore/pagestore.go
@@ -12,10 +12,10 @@ type PageStore struct {
 
 func (ps *PageStore) Add(storable store.Storable)                            {}
 func (ps *PageStore) Changed(storable store.Storable)                        {}
-func (ps *PageStore) Get(ident string) (storable store.Storable, found bool) { return }
-func (ps *PageStore) GetByIndex(indexNum int) []store.Storable               { return }
+func (ps *PageStore) Get(ident string) (storable store.Storable, found bool) { return nil, false }
+func (ps *PageStore) GetByIndex(indexNum int) []store.Storable               { return nil }
 func (ps *PageStore) Flag(flagNum int, storable store.Storable)              {}
-func (ps *PageStore) GetByFlagAndClear(flagNum int) []store.Storable         { return }
+func (ps *PageStore) GetByFlagAndClear(flagNum int) []store.Storable         { return nil }
 func (ps *PageStore) Close()                                                 {}
 
 type PersistentPageStore struct {
@@ -24,7 +24,7 @@ type PersistentPageStore struct {
 func (ps *PersistentPageStore) Add(storable store.Storable)                            {}
 func (ps *PersistentPageStore) Changed(storable store.Storable)                        {}
 func (ps *PersistentPageStore) Get(ident string) (storable store.Storable, found bool) { return }
-func (ps *PersistentPageStore) GetByIndex(indexNum int) []store.Storable               { return }
+func (ps *PersistentPageStore) GetByIndex(indexNum int) []store.Storable               { return nil }
 func (ps *PersistentPageStore) Flag(flagNum int, storable store.Storable)              {}
-func (ps *PersistentPageStore) GetByFlagAndClear(flagNum int) []store.Storable         { return }
+func (ps *PersistentPageStore) GetByFlagAndClear(flagNum int) []store.Storable         { return nil }
 func (ps *PersistentPageStore) Close()                                                 {}
--- src/pagestore/pagestore_test.go
+++ src/pagestore/pagestore_test.go
@@ -3,13 +3,58 @@ package pagestore
 import (
 	"store"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-// `TestFilePersist1` tests
-func TestPageStore1(t *testing.T) {
-	assert := assert.New(t)
-	s := &PageStore{}
+func makeTestPersistentPageStore() *PersistentPageStore { return &PersistentPageStore{} }
+
+// `TestPersistentPageStore1` tests
+func TestPersistentPageStore1(t *testing.T) {
+	s := makeTestPersistentPageStore()
 	store.TestStore1(s, t)
+}
+
+// `TestPersistentPageStore2` tests
+func TestPersistentPageStore2(t *testing.T) {
+	s := makeTestPersistentPageStore()
+	store.TestStore2(s, t)
+}
+
+// `TestPersistentPageStore3` tests
+func TestPersistentPageStore3(t *testing.T) {
+	s := makeTestPersistentPageStore()
+	store.TestStore3(s, t)
+}
+
+// `TestPersistentPageStore4` tests
+func TestPersistentPageStore4(t *testing.T) {
+	s := makeTestPersistentPageStore()
+	store.TestStore4(s, t)
+}
+
+func makeTestPageStore() *PageStore {
+	return &PageStore{backStore: makeTestPersistentPageStore()}
+}
+
+// `TestPageStore1` tests
+func TestPageStore1(t *testing.T) {
+	s := makeTestPageStore()
+	store.TestStore1(s, t)
+}
+
+// `TestPageStore2` tests
+func TestPageStore2(t *testing.T) {
+	s := makeTestPageStore()
+	store.TestStore2(s, t)
+}
+
+// `TestPageStore3` tests
+func TestPageStore3(t *testing.T) {
+	s := makeTestPageStore()
+	store.TestStore3(s, t)
+}
+
+// `TestPageStore4` tests
+func TestPageStore4(t *testing.T) {
+	s := makeTestPageStore()
+	store.TestStore4(s, t)
 }
